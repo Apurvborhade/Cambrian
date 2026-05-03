52f5f72fc2b1382a67d20637253278faa01f7c1d
Apurvborhade <apurvaborhadee@gmail.com>
2026-05-03 15:15:50 +0530

    proxy url updated for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://cambrian.apurvaborhade.dev",
         changeOrigin: true,
       },
     },
